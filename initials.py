5d4a1dffd195bbd80aa8f9203041846bb6bb3eab
Dj222005 <2425401011@live.gctu.edu.gh>
2026-05-30 12:19:05 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Daniel Yayra Cobblah
+# STUDENT ID:  2425401011
+# Email: 2425401011@live.gctu.edu.gh
+# Grader: Mr.Augustus Buckman
+#
+# On my honor, Daniel Yayra Cobblah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials DYC in small letters, then in large stylized block letters.
 
-
+    print()
+    print("...DYC")
+    print()
+    print("...DDDDDDDDD...........YYY......YYY.........CCCCCCCCCC......")
+    print("...DDDD....DDD.........YYY......YYY........CCC......CCC.....")
+    print("...DDD......DDD.........YYY....YYY.........CCC..............")
+    print("...DDD......DDD..........YYYYYYYYYY........CCC..............")
+    print("...DDD......DDD............YYYYYY..........CCC..............")
+    print("...DDD......DDD.............YYYY...........CCC..............")
+    print("...DDD......DDD.............YYYY...........CCC..............")
+    print("...DDD......DDD.............YYYY...........CCC..............")
+    print("...DDDD....DDD....**........YYYY......**...CCC......CCC...**")
+    print("...DDDDDDDDD......**........YYYY......**....CCCCCCCCCC....**")
+    print()
 
 main()
